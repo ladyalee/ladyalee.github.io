251866ac2226d1e5991da2e9ef52a84496faa15c
ladyalee <>
2019-04-08 13:29:40 -0500

    temple closure dates

--- assignments/Temple Site/scripts/templeinfo.js
+++ assignments/Temple Site/scripts/templeinfo.js
@@ -1,13 +1,17 @@
 var requestURL = "json/temples.json";
 var request = new XMLHttpRequest();
-request.open('GET', requestURL);        
+request.open('GET', requestURL, true);        
 request.responseType = 'json';
 request.send();
 request.onload = function() {
   var closures = request.response;
+
+console.log(closures);
+return;
+
+document.getElementById("closures")  
  
-closures = {
-  "dallasClosures",
-};
+
+
 }
 
