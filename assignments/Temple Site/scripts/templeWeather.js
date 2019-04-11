9bde2d461763d0f7112b0770c388d031933ef61f
ladyalee <>
2019-04-11 11:09:18 -0500

    added resources link

--- assignments/Temple Site/scripts/templeWeather.js
+++ assignments/Temple Site/scripts/templeWeather.js
@@ -9,5 +9,46 @@ dallasRequest.onload = function (){
     var dallasData = dallasRequest.response;
 
 console.log(dallasData);
+document.getElementById("current-temp1").innerHTML = dallasData.main.temp;
+}
 
+var nashvilleWeather = 'https://api.openweathermap.org/data/2.5/weather?zip=37069,us&appid=93935a5a252e185f91c54e43c8554d32&units=imperial';
+var nashvilleRequest = new XMLHttpRequest();
+
+nashvilleRequest.open('GET', nashvilleWeather, true);
+nashvilleRequest.responseType = 'json';
+nashvilleRequest.send();
+
+nashvilleRequest.onload = function (){
+    var nashvilleData = nashvilleRequest.response;
+
+console.log(nashvilleData);
+document.getElementById("current-temp2").innerHTML = nashvilleData.main.temp;
+}
+
+var mantiWeather = 'https://api.openweathermap.org/data/2.5/weather?zip=84642,us&appid=93935a5a252e185f91c54e43c8554d32&units=imperial';
+var mantiRequest = new XMLHttpRequest();
+
+mantiRequest.open('GET', mantiWeather, true);
+mantiRequest.responseType = 'json';
+mantiRequest.send();
+
+mantiRequest.onload = function (){
+    var mantiData = mantiRequest.response;
+
+console.log(mantiData);
+document.getElementById("current-temp3").innerHTML = mantiData.main.temp;
+}
+
+var billingsWeather = 'https://api.openweathermap.org/data/2.5/weather?zip=59106,us&appid=93935a5a252e185f91c54e43c8554d32&units=imperial';
+var billingsRequest = new XMLHttpRequest();
+
+billingsRequest.open('GET', billingsWeather, true);
+billingsRequest.responseType = 'json';
+billingsRequest.send();
+
+billingsRequest.onload = function (){
+    var billingsData = billingsRequest.response;
+
+document.getElementById("current-temp4").innerHTML = billingsData.main.temp;
 }
--- assignments/Temple Site/scripts/templeinfo.js
+++ assignments/Temple Site/scripts/templeinfo.js
@@ -6,14 +6,43 @@ request.send();
 request.onload = function() {
   var templeInfo = request.response;
 
-var closuresUL = document.createElement("ul");
-var closuresLI;
+var closuresUL1 = document.createElement("ul");
+var closuresLI1;
 
 for (var i = 0; i < templeInfo[0].closures.length; i++) {
-    closuresLI = document.createElement("li");
-    closuresLI.innerHTML = templeInfo[0].closures[i].start + " to " + templeInfo[0].closures[i].end;
-    closuresUL.appendChild(closuresLI);
+    closuresLI1 = document.createElement("li");
+    closuresLI1.innerHTML = templeInfo[0].closures[i].start + " to " + templeInfo[0].closures[i].end;
+    closuresUL1.appendChild(closuresLI1);
 }
+document.getElementById("templeClosures1").appendChild(closuresUL1);
 
-document.getElementById("templeClosures1").appendChild(closuresUL);
+var closuresUL2 = document.createElement("ul");
+var closuresLI2;
+
+  for (var i = 0; i < templeInfo[1].closures.length; i++) {
+      closuresLI2 = document.createElement("li");
+      closuresLI2.innerHTML = templeInfo[1].closures[i].start + " to " + templeInfo[1].closures[i].end;
+      closuresUL2.appendChild(closuresLI2);
+  }
+  document.getElementById("templeClosures2").appendChild(closuresUL2);
+ 
+  var closuresUL3 = document.createElement("ul");
+  var closuresLI3;
+  
+  for (var i = 0; i < templeInfo[2].closures.length; i++) {
+      closuresLI3 = document.createElement("li");
+      closuresLI3.innerHTML = templeInfo[2].closures[i].start + " to " + templeInfo[2].closures[i].end;
+      closuresUL3.appendChild(closuresLI3);
+  }
+  document.getElementById("templeClosures3").appendChild(closuresUL3);
+
+var closuresUL4 = document.createElement("ul");
+var closuresLI4;
+
+for (var i = 0; i < templeInfo[3].closures.length; i++) {
+    closuresLI4 = document.createElement("li");
+    closuresLI4.innerHTML = templeInfo[3].closures[i].start + " to " + templeInfo[3].closures[i].end;
+    closuresUL4.appendChild(closuresLI4);
+}
+document.getElementById("templeClosures4").appendChild(closuresUL4);
 }
